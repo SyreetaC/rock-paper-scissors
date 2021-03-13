4211cf3e358cdcc07d71cc13fad803ec8a9c1f6f
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-13 13:14:49 +0000

    initial js plan comments

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// declare global data
+
+// prompt player for choice
+
+//validate player choice
+
+//randomly generate computer choice
+
+//apply game logic to check who wins
+
+//update scores of winner/loser
+
+//display scores
+
+//present confirm option to play again
